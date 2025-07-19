6353e7adbf3ad4473978533f88b5179d163e1d13
VARUN <varunaltekar2@gmail.com>
2025-07-19 10:12:36 +0530

    for loops

---
+++ ForDemo.py
@@ -0,0 +1,26 @@
+# iterate string
+str1 = "abcdefgh"
+for k in str1:
+    print(k)
+
+# iterate in range
+for l in range(0,4):
+    print(l)
+
+#iterate using len
+for p in range(0,len(str1)):
+    print(p)
+
+# iterate list
+listNames = ["a","b","c"]
+for j in listNames:
+    print(j)
+
+# iterate tupple
+tupleNames = ("a","b","c")
+for m in tupleNames:
+    print(m)
+
+# 3rd param of range
+for e in range(1,16,2):
+    print(e)

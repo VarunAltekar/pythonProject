eb9e22431101bed9b4c95f4ab657fb88067a1883
VARUN <varunaltekar2@gmail.com>
2025-07-21 15:28:06 +0530

    function demo

---
+++ Function.py
@@ -0,0 +1,14 @@
+# function declaration & definition
+def isGreater(a, b):
+    if(a>b):
+        print("a is greater then b")
+    elif(a<b):
+        print("a is less than b")
+    else:
+        print("a & b are equal")
+
+# declare function, write body later
+def lazyFunc(a,b):
+    pass
+
+isGreater(2,3)
